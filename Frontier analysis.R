53ffbbee99e7763a19162dd047ed6efdc3993ea7
Bhargavi2288 <32273064+Bhargavi2288@users.noreply.github.com>
2018-08-30 19:34:19 -0400

    Delete Frontier analysis.R

--- Frontier analysis.R
+++
@@ -1,7 +0,0 @@
-install.packages("xlsx")
-require(xlsx)
-mydata<- read.csv("QualityMsrMDS_Download-12-11.csv")
-mydata
-dim(mydata)
-#### data for only Missouri state
-mydata_MO =mydata[mydata$STATE == "MO",]
